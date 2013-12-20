2413d59544e9e7cead8afa8b5ec379b17c8c6d6f
Maxime Demolin <akbarova.armia@gmail.com>
2013-12-20 18:55:15 +0100

    ADD gist posting
    What is a gist if we cannot post on it!
    Handle posting of some sample data.
    
    Details:
    * ADD passing params handling

--- gost.go
+++ gost.go
@@ -1,6 +1,8 @@
 package main
 import (
   "fmt"
+  "bytes"
+  "flag"
   "encoding/json"
   "net/http"
   "io/ioutil"
@@ -9,18 +11,22 @@ import (
   "time"
 )
 
+// get command line arguments
+var gistDescriptionFlag = flag.String("description", "", "Description of the gist")
+func init() {
+  flag.StringVar(gistDescriptionFlag, "d", "", "Description")
+}
 
 func main() {
+  flag.Parse()
+
   /* common variables */
-  base_url  := "https://api.github.com/"
-  var username string
-
-  fmt.Printf("Enter username\n")
-  fmt.Scanf("%s", &username)
-
-  url := base_url + "users/" + username + "/gists"
+  baseUrl  := "https://api.github.com/"
+  username := "MaximeD"
+  url := baseUrl + "users/" + username + "/gists"
 
   listGists(url)
+  postGist(baseUrl, *gistDescriptionFlag)
 }
 
 func listGists(url string) {
@@ -51,6 +57,37 @@ func listGists(url string) {
       }
     }
   }
+}
+
+type Gist struct {
+  Desc string `json:"description"`
+  Public bool `json:"public"`
+  Files map[string]GistFile `json:"files"`
+}
+
+type GistFile struct {
+  Content string `json:"content"`
+}
+
+func postGist(baseUrl string, description string) {
+  // add some content for tests
+  fileContent := "a test"
+  files := make(map[string]GistFile)
+  files["test.txt"] = GistFile{Content: fileContent}
+  gist := Gist{Desc: description, Public: true, Files: files}
+
+  // encode json
+  buf, err := json.Marshal(gist)
+  if err != nil {
+    fmt.Printf("%s", err)
+  }
+
+  // print our shiny new json
+  fmt.Println(string(buf))
+
+  body := bytes.NewBuffer(buf)
+  resp, err := http.Post(baseUrl + "gists", "text/json", body)
+  fmt.Println(resp)
 }
 
 func shortDate(date time.Time) string {
